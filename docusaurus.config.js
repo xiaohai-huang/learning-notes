b9fb613185521736433288f476bddc90b4294be7
xiaohai-huang <jeromehuang18@gmail.com>
2022-01-22 21:56:48 +0800

    fix broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -139,7 +139,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Xiaohai's Mind Palace, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,

0502c7e50002355229ae7bb151e5edbc562055e0
xiaohai-huang <jeromehuang18@gmail.com>
2022-01-24 17:23:55 +0800

    docs(JavaScript): add partial "this keyword"

--- docs/Web/JavaScript/_scripts/01-this-first.js
+++ docs/Web/JavaScript/_scripts/01-this-first.js
@@ -1,10 +1,27 @@
-function LogThis() {
-  console.log(this);
+function baz() {
+  // call-stack is: `baz`
+  // so, our call-site is in the global scope
+  console.log("baz");
+  bar(); // <-- call-site for `bar`
 }
 
-const obj = { name: "xiaohai" };
-obj.whatIsThis = LogThis;
+function bar() {
+  // call-stack is: `baz` -> `bar`
+  // so, our call-site is in `baz`
 
-console.log(this);
-LogThis();
-obj.whatIsThis();
+  console.log("bar");
+  foo(); // <-- call-site for `foo`
+}
+
+function foo() {
+  // call-stack is: `baz` -> `bar` -> `foo`
+  // so, our call-site is in `bar`
+
+  debugger; // remove this line to view the console output
+
+  console.log("foo");
+}
+
+baz(); // <-- call-site for `baz`
+
+// Retrieved from https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/this%20%26%20object%20prototypes/ch2.md
---
+++ src/pages/docs-todos.md
@@ -0,0 +1,8 @@
+# I am working hard on these missing knowlege
+
+They are unavailable now 🤭
+
+<!-- ✅ ❎ -->
+
+- ❎ execution context
+- ❎ call stack

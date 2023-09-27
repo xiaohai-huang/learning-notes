81186ff9cd3833b10dfae96807b372bd70a5ff32
xiaohai-huang <jeromehuang18@gmail.com>
2023-09-27 06:24:55 -0600

    fix: Check if the env is browser before init Aegis

--- src/aegis/init-script.js
+++ src/aegis/init-script.js
@@ -1,9 +1,13 @@
-import Aegis from "aegis-web-sdk";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-const aegis = new Aegis({
-  id: "kwL2lcKr1KJe8rXnlD", // 上报 id
-  uin: "", // 用户唯一 ID（可选）
-  reportApiSpeed: true, // 接口测速
-  reportAssetSpeed: true, // 静态资源测速
-  spa: true, // spa 应用页面跳转的时候开启 pv 计算
-});
+if (ExecutionEnvironment.canUseDOM) {
+  import("aegis-web-sdk").then(({ default: Aegis }) => {
+    const aegis = new Aegis({
+      id: "kwL2lcKr1KJe8rXnlD", // 上报 id
+      uin: "", // 用户唯一 ID（可选）
+      reportApiSpeed: true, // 接口测速
+      reportAssetSpeed: true, // 静态资源测速
+      spa: true, // spa 应用页面跳转的时候开启 pv 计算
+    });
+  });
+}
